894bd578287bd9136241b9284e9b33d3fb22113d
Damian Thomas <dimitrisonic@gmail.com>
2023-01-07 16:26:43 -0500

    successfully rendering sprites to canvas

--- src/components/RenderCanvas.tsx
+++ src/components/RenderCanvas.tsx
@@ -1,7 +1,23 @@
 import styles from "./RenderCanvas.module.scss";
+import { Application as PixiApp, ICanvas, Sprite } from "pixi.js";
+import { useCallback, useEffect, useState } from "react";
 
 export function RenderCanvas() {
-	return (
-		<div className={styles.Container}></div>	
-	);
+	const [pixi, loadPixiApp] = useState<PixiApp<ICanvas>>();
+
+	const view = useCallback((viewNode: HTMLCanvasElement) => {
+		if (viewNode && !pixi) {
+			loadPixiApp(new PixiApp({ width: 500, height: 500, view: viewNode }))
+		}
+	}, []);
+
+	useEffect(() => {
+		if (pixi) {
+			console.log("loading sprite..")
+			let sprite = Sprite.from('vite.svg');
+			pixi.stage.addChild(sprite);
+		}
+	}, [pixi])
+
+	return <canvas ref={view}></canvas>;
 }
